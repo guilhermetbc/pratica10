a9d355fa2943acd8b4b8d38fc2a94505d74f16f2
guilhermetbc <31928509+guilhermetbc@users.noreply.github.com>
2018-10-23 11:47:38 -0300

    Add files via upload

---
+++ praica11.py
@@ -0,0 +1,22 @@
+class Autor():
+  def __init__(self, nome):
+    self.primeironome = primeironome
+    self.nomedomeio = nomedomeio
+    self.ultimonome = ultimonome
+    self.datanascimento = datanascimento
+
+  def nome_como_citado(self):
+    return '{} {}.'.format(ultimonome.upper(), self.primeironome[0])
+
+  def __str__(self):
+
+
+class Livro():
+  self.titulo = titulo
+  self.ano = ano
+  self.autores = []
+
+class Biblioteca():
+  self.livros
+
+  def livros_por_autor(self):
